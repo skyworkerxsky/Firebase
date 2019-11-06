b8b689a883089ffd77053cc45ed5e7f56272ac65
Alexey Makarov <pwuser@yandex.ru>
2019-11-06 18:04:02 +0500

    Initial

---
+++ ToDo-Firebase/ToDo-Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo-Firebase
+//
+//  Created by Алексей on 06/11/2019.
+//  Copyright © 2019 Alexey Makarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
